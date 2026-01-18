6356440f6cea23b1ededbbfd3ffcecdd44e0f0b6
Anwar_Hossain <ranakrphone@gmail.com>
2026-01-18 21:15:24 +0600

    data ingesation component added

--- demo.py
+++ demo.py
@@ -1,14 +1,40 @@
+from us_visa.pipline.training_pipeline import TrainingPipeline
+
+pipeline = TrainingPipeline()
+pipeline.run_pipeline()
+
+
+
+# ---------------------------------------------------------------------
+
+
 # from us_visa.logger import logging
 
 # logging.info("Logging has started")
 
-from us_visa.exception import USvisaException
-from us_visa.logger import logging
-import sys
 
-try:
-    r = 10 / 0
-    print(r)
+# ---------------------------------------------------------------------
 
-except Exception as e:
-    raise USvisaException(e, sys)
+
+# from us_visa.exception import USvisaException
+# from us_visa.logger import logging
+# import sys
+
+# try:
+#     r = 10 / 0
+#     print(r)
+
+# except Exception as e:
+#     raise USvisaException(e, sys)
+
+# ---------------------------------------------------------------------
+
+# import os
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+# mongodb_url = os.getenv("MONGODB_URL")
+# print(mongodb_url)
+
+
